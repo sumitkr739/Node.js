c73f7a498a4548c038ed41f29e1ed07a9fec8cae
Sumit Kumar Sinha <sumitsinha739@gmail.com>
2024-03-18 22:07:45 +0530

    create mvc

--- index.js
+++ index.js
@@ -1,80 +1,26 @@
 const express = require("express");
-const fs = require("fs");
-const mongoose = require("mongoose");
-const users = require("./MOCK_DATA.json");
+const { connectMongoDb } = require("./connection");
+const { logReqRes } = require("./middlewares/index");
+const userRouter = require("./routes/user");
+
+// const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 //Connection
-mongoose.connect("mongodb://localhost:27017/NodeProject");
+connectMongoDb("mongodb://127.0.0.1:27017/Node_project").then(() =>
+  console.log("MongoDB Connected")
+);
+// .catch((err) => console.log("Mongo Error", err));
 
-//Schema
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  jobTitle: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
-
-const User = mongoose.model("user", userSchema);
+//Middleware - Plugin
+app.use(express.urlencoded({ extended: false }));
+app.use(logReqRes("log.txt"));
 
 //Routes
-app.get("/users", (req, res) => {
-  const HTML = `<ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `;
-  res.send(HTML);
-});
+app.use("/api/user", userRouter);
 
-//REST APIs
-app.get("/api/users", (req, res) => {
-  return res.json(users);
-});
-
-//Middleware
-app.use(express.urlencoded({ extended: false }));
-
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    res.json(user);
-  })
-  .patch((req, res) => {
-    //Edit user with id
-    res.json({ status: "pending" });
-  })
-  .delete((req, res) => {
-    //Delete user with id
-    res.json({ status: "pending" });
-  });
-
-app.post("/api/users", (req, res) => {
-  //Create new user
-  const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "success", id: users.length });
-  });
-});
-
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`server started at port ${PORT}`);
 });
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+} = require("../controllers/user");
+
+const router = express.Router();
+
+//REST APIs
+router.route("/").get(handleGetAllUsers).post(handleCreateNewUser);
+
+router
+  .route("/:id")
+  .get(handleGetUserById)
+  .patch(handleUpdateUserById)
+  .delete(handleDeleteUserById);
+
+module.exports = router;
